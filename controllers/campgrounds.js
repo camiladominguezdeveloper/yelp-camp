21b84f2b9b9e5127ef20d96d83a198ed046e9095
Camila Dominguez <127351679+camiladominguezdeveloper@users.noreply.github.com>
2025-01-28 11:16:50 -0300

    Add maps with maptiler api

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,7 @@
 const Campground = require('../models/campground');
 const {cloudinary} = require("../cloudinary");
+const maptilerClient = require("@maptiler/client");
+maptilerClient.config.apiKey = process.env.MAPTILER_API_KEY;
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({})
@@ -11,7 +13,10 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async (req, res, next) => {
+    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
     const campground = new Campground(req.body.campground);
+    console.log(geoData);
+    campground.geometry = geoData.features[0].geometry;
     campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();
@@ -46,6 +51,8 @@ module.exports.renderEditForm = async (req, res) => {
 module.exports.updateCampground = async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
+    campground.geometry = geoData.features[0].geometry;
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
     await campground.save();
--- seeds/index.js
+++ seeds/index.js
@@ -15,13 +15,19 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             author: '67765e0c2a22db272dc04fc6',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero veniam eligendi rem quibusdam maxime ipsa adipisci modi qui iste? Esse, quas. Hic sit, aliquam saepe ab exercitationem esse consequatur.',
+            price,
+            geometry: {
+                type: 'Point',
+                coordinates: [cities[random1000].longitude, cities[random1000].latitude]
+            },
             images: [
                 {
                 url: 'https://res.cloudinary.com/dzultoj5g/image/upload/v1736789669/YelpCamp/o1msh9n7espg6miiu5lp.jpg',
@@ -31,9 +37,7 @@ const seedDB = async () => {
                 url: 'https://res.cloudinary.com/dzultoj5g/image/upload/v1736789669/YelpCamp/zpjlqcpwsuwui9tykfor.jpg',
                 filename: 'YelpCamp/zpjlqcpwsuwui9tykfor',
                 }
-            ],
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero veniam eligendi rem quibusdam maxime ipsa adipisci modi qui iste? Esse, quas. Hic sit, aliquam saepe ab exercitationem esse consequatur.',
-            price
+            ]
         })
         await camp.save();
     }
